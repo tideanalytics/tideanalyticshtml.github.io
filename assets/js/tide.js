6d3fe440d51f38acd921e608a5ca465f27d21352
Zi-Yuan <ziyuan@tideanalytics.com>
2019-04-02 19:48:53 +0800

    update company logo orientation

--- assets/js/tide.js
+++ assets/js/tide.js
@@ -1,3 +1,19 @@
+
+/*---------------------------------
+sticky header JS
+-----------------------------------*/
+$(window).on('scroll',function() {
+  'use strict';
+  let scroll = $(window).scrollTop();
+  if (scroll < 100) {
+    document.getElementById('tide-logo').src = 'internal-assets/img/logo-vertical-grey.png';
+    $('#header_sticky').removeClass('sticky');
+  } else {
+    document.getElementById('tide-logo').src = 'internal-assets/img/logo-horizontal-grey.png';
+    $('#header_sticky').addClass('sticky');
+  }
+});
+
 (function() {
   'use strict';
   const year = new Date().getFullYear();

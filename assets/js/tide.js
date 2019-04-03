cc4c0c07d7a8570f78556f5ae5f2d992e89ca3a9
Zi-Yuan <ziyuan@tideanalytics.com>
2019-04-03 15:54:21 +0800

    add freshchat engagement tool

--- assets/js/tide.js
+++ assets/js/tide.js
@@ -1,6 +1,5 @@
 
 // overwrite template script
-
 /*---------------------------------
 sticky header JS
 -----------------------------------*/
@@ -17,9 +16,28 @@ $(window).on('scroll',function() {
 });
 
 // get latest year
-
 (function() {
   'use strict';
   const year = new Date().getFullYear();
   document.getElementById('footer_year').innerHTML = year;
 })();
+
+// ga
+(function() {
+  'use strict';
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-47472966-1');
+})();
+
+
+// freshchat
+(function() {
+  'use strict';
+  window.fcWidget.init({
+    token: "782ca078-53a3-430c-86e9-f06c6a1f1aa0",
+    host: "https://wchat.freshchat.com"
+  });
+})();
+

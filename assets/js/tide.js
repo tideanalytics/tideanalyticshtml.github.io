d8a03924132810bf8a94622e4d88283f30849b60
Zi-Yuan <ziyuan@tideanalytics.com>
2019-04-03 17:46:26 +0800

    make freshchat async

--- assets/js/tide.js
+++ assets/js/tide.js
@@ -1,9 +1,8 @@
-
 // overwrite template script
 /*---------------------------------
 sticky header JS
 -----------------------------------*/
-$(window).on('scroll',function() {
+$(window).on('scroll', function () {
   'use strict';
   let scroll = $(window).scrollTop();
   if (scroll < 100) {
@@ -16,28 +15,43 @@ $(window).on('scroll',function() {
 });
 
 // get latest year
-(function() {
+(function () {
   'use strict';
   const year = new Date().getFullYear();
   document.getElementById('footer_year').innerHTML = year;
 })();
 
 // ga
-(function() {
+(function () {
   'use strict';
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag() {dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', 'UA-47472966-1');
 })();
 
 
 // freshchat
-(function() {
+(function () {
   'use strict';
-  window.fcWidget.init({
-    token: "782ca078-53a3-430c-86e9-f06c6a1f1aa0",
-    host: "https://wchat.freshchat.com"
-  });
-})();
+  function initFreshChat() {
+    window.fcWidget.init({
+      token: "782ca078-53a3-430c-86e9-f06c6a1f1aa0",
+      host: "https://wchat.freshchat.com"
+    });
+  }
 
+  (function (d, id) {
+    var fcJS;
+    if (d.getElementById(id)) {
+      initFreshChat();
+      return;
+    }
+    fcJS = d.createElement('script');
+    fcJS.id = id;
+    fcJS.async = true;
+    fcJS.src = 'https://wchat.freshchat.com/js/widget.js';
+    fcJS.onload = initFreshChat;
+    d.head.appendChild(fcJS);
+  }(document, 'freshchat-js-sdk'));
+})();
